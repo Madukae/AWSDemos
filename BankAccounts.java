644ea4d4503ba1c4f61c0e39081cb882de0f9ced
Madukae <40549516+Madukae@users.noreply.github.com>
2018-07-02 17:41:15 -0400

    Update BankAccounts.java

--- BankAccounts.java
+++ BankAccounts.java
@@ -1,1 +1,20 @@
-https://github.com/Madukae/JenkinsDemo.git
+package ca.uwo.csd.cs2212.USERNAME;
+
+public class BankAccount {
+
+  private double balance;
+
+  public BankAccount(double balance) {
+    this.balance = balance;
+  }
+
+  public double debit(double amount) {
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount;
+    return amount;
+  }
+
+}
